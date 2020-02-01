91d13972efd3ae1969ba495dbc344265be97faa8
Michael Kilfeather <michaelkilfeather94@gmail.com>
2020-02-01 22:08:43 +0000

    Code cleanup

--- ExciteQTE/Assets/SCRIPTS/ButtonManager.cs
+++ ExciteQTE/Assets/SCRIPTS/ButtonManager.cs
@@ -10,14 +10,14 @@ public class ButtonManager : MonoBehaviour {
 
     public AudioClip[] sounds;
 
-   private AudioSource soundManager;
+    private AudioSource soundManager;
 
     public AudioSource music;
 
     public Slider timeSlider;
     public RectTransform newfillRect;
-    public float timeAmt; //was float
-    float time; // was float
+    public float timeAmt; 
+    float time; 
 
 
     public Text sliderScoreText;
@@ -37,21 +37,19 @@ public class ButtonManager : MonoBehaviour {
     Animator anim;
 
     [SerializeField]
-   public InputDevice device;
-
+    public InputDevice device;
+ 
     public GameObject GameOverobj;
 
     public GameObject[] ScoreTexts;
 
 
-   const int sliderValue = 100;
-
+    const int sliderValue = 100;
 
     public bool canPlay;
     public bool gameEnd;
 
 
-    // Use this for initialization
     void Awake()
     {
         soundManager = GetComponent<AudioSource>();
@@ -61,31 +59,21 @@ public class ButtonManager : MonoBehaviour {
 
         device = InputManager.ActiveDevice;
 
-
-
     }
 
     void Start()
     {
-     //   newfillRect = timeSlider.fillRect;
         time = timeAmt;
         timeSlider.value = sliderValue;
-
-        
-        ImageLoad();
-      
-        
-     
+    
+        ImageLoad(); 
     }
 
   
 	
 	void Update ()
     {
-      
-       
         QTEMode();
-       
     }
 
     
@@ -112,337 +100,96 @@ public class ButtonManager : MonoBehaviour {
 
     void QTEMode()
     {
-       // if (buttonImg.IsActive())
+
        if(canPlay)
         {
             if (InputManager.ActiveDevice.Action2.WasPressed && buttonImg.sprite == buttons[0])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-
-                if (buttonPresses <= 0) // newly added
-                {
-                   
-                    StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-                }
-                else // newly added
-                {
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    timeSlider.value = 100;
-                    ImageLoad();
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
                    
             else if (InputManager.ActiveDevice.Action2.WasPressed && buttonImg.sprite != buttons[0])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
-                //play wrong sound
-                //gain no points / lose points
-                //fire method to pause, before loading new image
+                WrongInput();
             }
 
             if (InputManager.ActiveDevice.Action1.WasPressed && buttonImg.sprite == buttons[1])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-                // StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-
-                if (buttonPresses <= 0) // newly added
-                {
-                  
-                    StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-                }
-                else // newly added
-                {
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    timeSlider.value = 100;
-                    ImageLoad();
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
 
             else if (InputManager.ActiveDevice.Action1.WasPressed && buttonImg.sprite != buttons[1])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
+                WrongInput();
             }
 
             if (InputManager.ActiveDevice.Action3.WasPressed && buttonImg.sprite == buttons[2])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-                //  StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-
-
-                if (buttonPresses <= 0) // newly added
-                {
-                   
-                    StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-                }
-                else // newly added
-                {
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                   
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    timeSlider.value = 100;
-                    ImageLoad();
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
 
             else if (InputManager.ActiveDevice.Action3.WasPressed && buttonImg.sprite != buttons[2])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
+                WrongInput();
             }
 
             if (InputManager.ActiveDevice.Action4.WasPressed && buttonImg.sprite == buttons[3])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-                //  StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-
-                if (buttonPresses <= 0) // newly added
-                {
-                   
-                    StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-                }
-                else // newly added
-                {
-                    //add score
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    timeSlider.value = 100;
-                    ImageLoad();
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
 
             else if (InputManager.ActiveDevice.Action4.WasPressed && buttonImg.sprite != buttons[3])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
+                WrongInput();
             }
 
 
             if (InputManager.ActiveDevice.DPadUp.WasPressed && buttonImg.sprite == buttons[4])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-
-                if (buttonPresses <= 0) // newly added
-                {
-                    StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-                }
-                else // newly added
-                {
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    timeSlider.value = 100;
-                    ImageLoad();
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
 
             else if (InputManager.ActiveDevice.DPadUp.WasPressed && buttonImg.sprite != buttons[4])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
-                //play wrong sound
-                //gain no points / lose points
-                //fire method to pause, before loading new image
+                WrongInput();
             }
 
 
             if (InputManager.ActiveDevice.DPadDown.WasPressed && buttonImg.sprite == buttons[5])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-
-                if (buttonPresses <= 0) // newly added
-                {
-                    StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-                }
-                else // newly added
-                {
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    timeSlider.value = 100;
-                    ImageLoad();
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
 
             else if (InputManager.ActiveDevice.DPadDown.WasPressed && buttonImg.sprite != buttons[5])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
-                //play wrong sound
-                //gain no points / lose points
-                //fire method to pause, before loading new image
+                WrongInput();
             }
 
 
             if (InputManager.ActiveDevice.DPadLeft.WasPressed && buttonImg.sprite == buttons[6])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-
-                if (buttonPresses <= 0) // newly added
-                {
-                    StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
-                }
-                else // newly added
-                {
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    timeSlider.value = 100;
-                    ImageLoad();
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
 
             else if (InputManager.ActiveDevice.DPadLeft.WasPressed && buttonImg.sprite != buttons[6])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
-                //play wrong sound
-                //gain no points / lose points
-                //fire method to pause, before loading new image
+                WrongInput();
             }
 
 
 
             if (InputManager.ActiveDevice.DPadRight.WasPressed && buttonImg.sprite == buttons[7])
             {
-                Debug.Log("Correct");
-                soundManager.PlayOneShot(sounds[1]);
-
-                if (buttonPresses <= 0) // newly added
-                {
-                    StartCoroutine(Correct()); 
-                }
-                else // newly added
-                {
-                    buttonPresses--;
-                    //  scoreManage.pointsToBeAdded += 1000 + timeSlider.value * 100f;
-                    scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
-                    ImageLoad();
-                    timeSlider.value = 100;
-                    time = timeAmt;
-                }
-
-
+                CorrectInput();
             }
 
             else if (InputManager.ActiveDevice.DPadRight.WasPressed && buttonImg.sprite != buttons[7])
             {
-                Debug.Log("WRONG!");
-                if (scoreManage.lives <= 1)
-                {
-                    StartCoroutine(GameOver());
-
-                }
-                else
-                {
-                    StartCoroutine(Wrong());
-                }
-                //play wrong sound
-                //gain no points / lose points
-                //fire method to pause, before loading new image
+                WrongInput();
             }
-
-
-
-
-
-
 
             Timer();
            
-
-
         }
 
         else
@@ -451,18 +198,58 @@ public class ButtonManager : MonoBehaviour {
         }
     }
 
+
+    //Button input matches button icon on screen
+    private void CorrectInput()
+    {
+        soundManager.PlayOneShot(sounds[1]);
+
+        if (buttonPresses <= 0)
+        {
+            StartCoroutine(Correct());  //fire method to pause, before loading new image (ienumerator) Play correct sound //gain points
+        }
+        else
+        {
+            buttonPresses--;
+            scoreManage.pointsToBeAdded += pointNum + timeSlider.value;
+            timeSlider.value = 100;
+            ImageLoad();
+            time = timeAmt;
+        }
+
+    }
+
+    //Button input does not match button icon on screen
+    private void WrongInput()
+    {
+        if (scoreManage.lives <= 1)
+        {
+            StartCoroutine(GameOver());
+
+        }
+        else
+        {
+            StartCoroutine(Wrong());
+        }
+    }
+
+
+
+
+
+
+
+
     void Timer()
     {
         if (time > 0)
         {
-            //time -= Time.deltaTime;
-            //  timeSlider.value = time / timeAmt;    
             timeSlider.value -= timeAmt;   
         }
 
         if(timeSlider.value <= 0)
         {
-            if (scoreManage.lives <= 1) // was 0
+            if (scoreManage.lives <= 1)
             {
                 StartCoroutine(GameOver());
                
@@ -475,15 +262,14 @@ public class ButtonManager : MonoBehaviour {
         }
     }
 
-    void SequenceChooser()  //TODO
+    void SequenceChooser()
     {
-        //random
         sequenceNum = Random.Range(1, 5);
         buttonPresses = sequenceNum;
     }
 
 
-    void ButtonAnimToggle(bool enabled) // bool enabled
+    void ButtonAnimToggle(bool enabled)
     {
         if (enabled)
         {
@@ -504,15 +290,12 @@ public class ButtonManager : MonoBehaviour {
         time = timeAmt;
         timeSlider.value = 100;
         sliderScoreText.text = "";
-        canPlay = true; // NEW
+        canPlay = true;
         ButtonAnimToggle(enabled);
-
-      //  scoreManage.prevHiScore = highscores.highscoresList[0].score;
-    //    scoreManage.hiscoreText.text = scoreManage.prevHiScore.ToString();
 
         if (gameEnd)
         {
-            scoreManage.ScoreTextRefresh(); // originally commented
+            scoreManage.ScoreTextRefresh();
             scoreManage.mid = false;
             scoreManage.sup = false;
             scoreManage.exp = false;
@@ -528,7 +311,6 @@ public class ButtonManager : MonoBehaviour {
                 img.gameObject.SetActive(true);
             }
             music.Play();
-            //scoreManage.ScoreTextRefresh(); //originally uncommented
             scoreManage.ScoreTextHideShow(true);
             
         }
@@ -540,8 +322,6 @@ public class ButtonManager : MonoBehaviour {
     IEnumerator Correct()
     {
 
-        // scoreManage.Score += 1000 + timeSlider.value * 100f + scoreManage.pointsToBeAdded;
-        // scoreManage.Score += 1000 + timeSlider.value + scoreManage.pointsToBeAdded;
         if(scoreManage.difficultyLvl <= ScoreManager.Difficulty.Middle)
         {
             scoreManage.Score += pointNum + timeSlider.value;
@@ -566,9 +346,7 @@ public class ButtonManager : MonoBehaviour {
         yield return new WaitForSeconds(1.5f);
 
         Init();
-      
 
-     //   timeSlider.fillRect.rect.xMax
     }
 
     IEnumerator Wrong()
@@ -584,10 +362,9 @@ public class ButtonManager : MonoBehaviour {
         scoreManage.pointsToBeAdded = 0f;
         yield return new WaitForSeconds(2.0f);
         soundManager.PlayOneShot(sounds[3]);
-        scoreManage.lifeImgs[scoreManage.lifeIndex].GetComponent<Animation>().Play();//animate life icon
+        scoreManage.lifeImgs[scoreManage.lifeIndex].GetComponent<Animation>().Play();
         yield return new WaitForSeconds(4f);
-        scoreManage.lifeImgs[scoreManage.lifeIndex].gameObject.SetActive(false);
-        // scoreManage.lifeImgs[scoreManage.lifeIndex].enabled = false;        
+        scoreManage.lifeImgs[scoreManage.lifeIndex].gameObject.SetActive(false);     
         music.Play();
 
         Init();
@@ -595,34 +372,13 @@ public class ButtonManager : MonoBehaviour {
     }
 
 
-  
-
-    /*
-    IEnumerator Wrong()
-    {
-
-        music.Stop(); //stop music
-        scoreManage.lives--;
-        scoreManage.lifeIndex = scoreManage.lives;
-        scoreManage.lifeImgs[scoreManage.lifeIndex].enabled = false;
-        soundManager.PlayOneShot(sounds[2]);
-        ButtonAnimToggle(!enabled);
-        ImageLoad();
-        yield return new WaitForSeconds(2.0f);
-        music.Play();
-        time = timeAmt;
-        ButtonAnimToggle(enabled);
-
-    }
-    */
-
     IEnumerator GameOver()
     {
         Debug.Log("Game Over pal");
         scoreManage.lives--;
         ButtonAnimToggle(!enabled);
         canPlay = false;
-        music.Stop();//stop music
+        music.Stop();
         soundManager.PlayOneShot(sounds[2]); 
         yield return new WaitForSeconds(2.0f);
         soundManager.PlayOneShot(sounds[3]);
@@ -633,11 +389,7 @@ public class ButtonManager : MonoBehaviour {
         timeSlider.gameObject.SetActive(false);
         scoreManage.HiScoreCheckEnd();
         gameEnd = true;
-      //  buttonImg.enabled = false;
-     
-       
-     
-
+   
     }
 
 
